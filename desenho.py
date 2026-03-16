76727cf4cbf6aba55df6cec148c4d31f1ff66367
Rafa <rraaffaaeellaa2009@gmail.com>
2026-03-16 15:14:34 -0300

    mudei o nome

--- desenho.py
+++ desenho.py
@@ -6,7 +6,7 @@ img= np.zeros((512,512,3), np.uint8)
 cv.line(img, (0,250), (500,250), (255,0,0),5)
 
 font=cv.FONT_HERSHEY_DUPLEX
-cv.putText(img,'OpenCV',(10,240), font, 4, (255,255,255),2, cv.LINE_AA)
+cv.putText(img,'RAFA',(10,240), font, 4, (255,255,255),2, cv.LINE_AA)
 cv.namedWindow ('carregando a imagem')
 cv.imshow('carregando a imagem', img) 
 k=cv.waitKey()
